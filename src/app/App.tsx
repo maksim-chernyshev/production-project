2b3652a638efafda97d0b48f3a5f6bb1cc6091df
maksim_chernyshev <maksim.tch@gmail.com>
2024-01-07 11:48:38 +0400

    add i18next, add themes, add babel, add extract plugin

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import {Link, Route, Routes} from "react-router-dom";
+import './styles/index.scss'
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
+
+export const App = () => {
+    const {theme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback=''>
+                <Navbar/>
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter/>
+                </div>
+            </Suspense>
+        </div>
+    );
+};
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,25 @@
+import {Route, Routes} from "react-router-dom";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {Suspense} from "react";
+import {routeConfig} from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+    return (
+        <div className='page-wrapper'>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {Object.values(routeConfig).map(({path, element}) => (
+
+                        <Route
+                            key={path}
+                            path={path}
+                            element={element}
+                        />
+
+                        ))}
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
