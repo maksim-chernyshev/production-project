231166922d0d101a6180aae284b49c852789208b
maksim_chernyshev <maksim.tch@gmail.com>
2024-01-31 22:16:34 +0400

    Fix ESLint config, Fix TSConfig, Fix i18n error

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,28 +1,33 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink
-                className={cls.mainLink}
-                theme={AppLinkTheme.SECONDARY}
-                to="/"
-            >
-                Главная
-            </AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
 
-            <AppLink
-                theme={AppLinkTheme.SECONDARY}
-                to="/about"
-            >
-                О сайте
-            </AppLink>
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    className={cls.mainLink}
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/"
+                >
+                    {t('Главная')}
+                </AppLink>
+
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/about"
+                >
+                    {t('О сайте')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
