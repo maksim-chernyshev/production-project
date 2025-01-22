45af2f29f89034d89240219ef323a8e9421305c9
Maksim Chernyshev <m.chernyshev@kefirgames.com>
2025-01-22 22:42:57 +0400

    Fix sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,33 +1,36 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 
+import { useSelector } from 'react-redux';
+import { getSidebarItems } from 'widgets/Sidebar/model/selectors/getSidebarItems';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
-import { SidebarItemsList } from '../../model/items';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
-    className?: string;
+  className?: string;
 }
 
-export const Sidebar = memo((props: SidebarProps) => {
-    const { className } = props;
+export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const sidebarItemsList = useSelector(getSidebarItems);
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
+    const itemsList = useMemo(() => sidebarItemsList.map((item) => (
+        <SidebarItem item={item} collapsed={collapsed} key={item.path} />
+    )), [collapsed, sidebarItemsList]);
+
     return (
         <div
             data-testid="sidebar"
-            className={classNames(
-                cls.Sidebar,
-                { [cls.collapsed]: collapsed },
-                [className],
-            )}
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+                className,
+            ])}
         >
             <Button
                 data-testid="sidebar-toggle"
@@ -42,21 +45,12 @@ export const Sidebar = memo((props: SidebarProps) => {
             </Button>
 
             <div className={cls.items}>
-                {SidebarItemsList.map((item) => (
-                    <SidebarItem
-                        item={item}
-                        collapsed={collapsed}
-                        key={item.path}
-                    />
-                ))}
+                {itemsList}
             </div>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher
-                    className={cls.lang}
-                    short={collapsed}
-                />
+                <LangSwitcher className={cls.lang} short={collapsed} />
             </div>
         </div>
     );
--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,37 +1,35 @@
 import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { SidebarItemType } from 'widgets/Sidebar/model/items';
 import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from 'entities/User';
+import { SidebarItemType } from 'widgets/Sidebar/model/types/sidebar';
 import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
-    item: SidebarItemType;
-    collapsed: boolean
+  item: SidebarItemType;
+  collapsed: boolean;
 }
 
-export const SidebarItem = memo(
-    (props: SidebarItemProps) => {
-        const { item, collapsed } = props;
-        const { t } = useTranslation();
+export const SidebarItem = memo((props: SidebarItemProps) => {
+    const { item, collapsed } = props;
+    const { t } = useTranslation();
 
-        const isAuth = useSelector(getUserAuthData);
+    const isAuth = useSelector(getUserAuthData);
 
-        if (item.authOnly && !isAuth) {
-            return null;
-        }
+    if (item.authOnly && !isAuth) {
+        return null;
+    }
 
-        return (
-            <AppLink
-                className={classNames(cls.item, { [cls.collapsed]: collapsed })}
-                theme={AppLinkTheme.SECONDARY}
-                to={item.path}
-            >
-                <item.Icon className={cls.icon} />
-                <span className={cls.link}>{t(item.text)}</span>
-            </AppLink>
-        );
-    },
-);
+    return (
+        <AppLink
+            className={classNames(cls.item, { [cls.collapsed]: collapsed })}
+            theme={AppLinkTheme.SECONDARY}
+            to={item.path}
+        >
+            <item.Icon className={cls.icon} />
+            <span className={cls.link}>{t(item.text)}</span>
+        </AppLink>
+    );
+});
